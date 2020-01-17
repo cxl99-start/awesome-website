6e9ebb96f5f756845bd65818287263b498217a9c
e <1620657798@qq.com>
2020-01-17 15:07:36 +0800

    修改test.py文件中的test函数，新增判断；上传项目文件（.idea文件夹）

--- www/test.py
+++ www/test.py
@@ -1,7 +1,15 @@
-import www.orm as orm
+import www.orm as orma
 import asyncio
 from www.models import User,Blog,Comment
 
 async def test(loop):
-    await orm.create_pool(loop=loop,user='root',password='root',db='awesome')
-    U=User(name='Test',email='test@qq.com',)
+    await orma.create_pool(loop=loop,host='localhost', user='www-data', password='www-data', db='awesome')
+    u = User(name='Test', email='test@qq.com', passwd='1234567890', image='about:blank')
+    await u.save()
+    #添加到数据库后需要关闭连接池，否则会报错 RuntimeError: Event loop is closed
+    orma.__pool.close()
+    await orma.__pool.wait_closed()
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(test(loop))
+    loop.close()
